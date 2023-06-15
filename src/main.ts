8fa0915414920baf6e779599940e97063a8487a0
BGGBGIBS <83280831+BGGBGIBS@users.noreply.github.com>
2023-06-15 13:05:09 +0200

    gitenv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+const env = process.env.NODE_ENV || 'development';
+config({ path: `.env.${env}` });
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
